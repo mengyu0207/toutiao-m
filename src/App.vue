5ffa7522f45b005679dc8ddf2d646e1ba4f9bebb
YGWW <1540612235@qq.com>
2022-07-13 12:07:36 +0800

    晌午

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="app">
+ <div id="app">
+    <router-view/>
     <h1>宇哥威武</h1>
-    <router-view />
-  </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<router-view></router-view>
+    <i class="toutiao toutiao-lishi">hhh</i>
+</div>
 </template>
 
 <script>
